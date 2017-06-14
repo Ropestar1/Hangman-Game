403e181d93089b0ea1dc553ae7af2b7fbd4e7453
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-06-14 00:00:49 -0700

    Figuring out coding condition for when first key is pressed, it should start the game vs. when the game is being played.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,31 @@
 //opening codes to start-initialize the game
-gameStart();
+//Initial arrays and variables. Resets at game over.
+var arrSelectedWord = ["piggy"];
+var arrLetterBank = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var arrWrongLetters = [];
+var arrCorrectLetters = [];
 
-function gameStart(anyKey) {
-	//creates empty arrays
-	var arrSelectedWord = ["piggy"];
-	var arrGuessedLetters = [];
-	var arrWrongLetters = [];
-	var arrCorrectLetters = [];
+var winCount = 0;
+var lossCount = 0;
+var guessCount = 0;
 
-	console.log("arrSelectedWord", arrSelectedWord);
-	console.log("arrGuessedWord", arrGuessedLetters);
-	console.log("arrWrongLetters", arrWrongLetters);
-	console.log("arrCorrectedLetters", arrCorrectLetters);
-	// function gamePlay(userLetters) {
+console.log("arrSelectedWord", arrSelectedWord);
+console.log("arrGuessedWord", arrGuessedLetters);
+console.log("arrWrongLetters", arrWrongLetters);
+console.log("arrCorrectedLetters", arrCorrectLetters);
+console.log("winCount", winCount);
+console.log("lossCount", lossCount);
+console.log("guessCount", guessCount);
 
-	// }
+
+//document.getElementById("starter").onkeypress = function() {gameStart()};
+if (winCount === 0 && lossCount === 0 && guessCount === 0){
+	document.onkeyup = function gameStart() {
+		console.log(event.key);
+
+		var startText = document.getElementById("starter");
+		startText.innerHTML = "Game on!";
+		}
+
+else if ()
 }
